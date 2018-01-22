2834cb3bf1b0411fd349341b98dddf10f2610469
zenghongyujia123 <1963968619@qq.com>
2018-01-22 11:28:17 +0800

    Set up

--- server/controllers/page_wechat.server.controller.js
+++ server/controllers/page_wechat.server.controller.js
@@ -8,9 +8,43 @@ var moment = require('moment');
 var cookieLib = require('../../libraries/cookie');
 var agent = require('superagent').agent();
 
+
+
+
 exports.page_home = function (req, res, next) {
   var cookie = cookieLib.getCookie(req);
   var username = cookie.userName;
+  var user = req.user;
   var filepath = path.join(__dirname, '../../web/c_wechat/views/page_home.client.view.html');
   return res.render(filepath, {});
+
 };
+
+
+exports.page_signin = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_signin.client.view.html');
+  return res.render(filepath, {});
+
+  // wechatLogic.getUserAccessToken(req.query.code, function (err, result) {
+  //   if (result.openid) {
+  //     cookieLib.setCookie(res, 'openid', result.openid);
+  //     cookieLib.setCookie(res, 'user_access_token', result.access_token);
+
+  //     shippmentLogic.getUserWechatInfo(result.openid, function (err, userWechat) {
+  //       if (userWechat) {
+  //         var filepath = path.join(__dirname, '../../web/c_wechat/views/page_signin.client.view.html');
+  //         return res.render(filepath, { username: userWechat.username, password: userWechat.password });
+  //       }
+  //       else {
+  //         var filepath = path.join(__dirname, '../../web/c_wechat/views/page_signin.client.view.html');
+  //         return res.render(filepath, { username: '', password: '' });
+  //       }
+  //     });
+  //   }
+  //   else {
+  //     var filepath = path.join(__dirname, '../../web/c_wechat/views/page_signin.client.view.html');
+  //     return res.render(filepath, { username: '', password: '' });
+  //   }
+  // });
+};
+
--- server/routes/page_wechat.server.route.js
+++ server/routes/page_wechat.server.route.js
@@ -7,13 +7,5 @@ var userFilter = require('../filters/user');
 
 module.exports = function (app) {
   app.route('/page_wechat/page_home').get(userFilter.requireUser, index.page_home);
-  app.route('/page_wechat/page_detail').get(index.page_detail);
-  app.route('/page_wechat/page_expense_list').get(index.page_expense_list);
-  app.route('/page_wechat/page_expense_detail').get(index.page_expense_detail);
   app.route('/page_wechat/page_signin').get(index.page_signin);
-
-  app.route('/page_wechat/page_expense_add').get(index.page_expense_add);
-  app.route('/page_wechat/page_profile').get(index.page_profile);
-  app.route('/page_wechat/page_search').get(index.page_search);
-  app.route('/page_wechat/page_setting').get(index.page_setting);
 };
