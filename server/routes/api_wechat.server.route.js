0e14d1f5f35bd5a36d61fc49099d641f729d7cac
zenghongyujia123 <1963968619@qq.com>
2018-01-29 22:15:45 +0800

    Update for test

--- server/routes/api_wechat.server.route.js
+++ server/routes/api_wechat.server.route.js
@@ -8,11 +8,11 @@ var userFilter = require('../filters/user');
 module.exports = function (app) {
   app.route('/api_wechat/signin').post(ctr.signin);
   // app.route('/api_wechat/shippments').post(ctr.shippments);
-  app.route('/api_wechat/uploadEvent').post(ctr.uploadEvent);
-  app.route('/api_wechat/createExpense').post(ctr.createExpense);
-  app.route('/api_wechat/getDeliveriedShippments').post(userFilter.requireUser, ctr.getDeliveriedShippments);
-  app.route('/api_wechat/getUserJsApiTicket').post(ctr.getUserJsApiTicket);
-  app.route('/api_wechat/downloadPhoto').get(ctr.downloadPhoto);
-  app.route('/api_wechat/updateUserSetting').post(ctr.updateUserSetting);
-  app.route('/api_wechat/getUserSetting').post(ctr.getUserSetting);
+  // app.route('/api_wechat/uploadEvent').post(ctr.uploadEvent);
+  // app.route('/api_wechat/createExpense').post(ctr.createExpense);
+  // app.route('/api_wechat/getDeliveriedShippments').post(userFilter.requireUser, ctr.getDeliveriedShippments);
+  // app.route('/api_wechat/getUserJsApiTicket').post(ctr.getUserJsApiTicket);
+  // app.route('/api_wechat/downloadPhoto').get(ctr.downloadPhoto);
+  // app.route('/api_wechat/updateUserSetting').post(ctr.updateUserSetting);
+  // app.route('/api_wechat/getUserSetting').post(ctr.getUserSetting);
 };
