8b01c1e8c193d0374552ea53dfc2351bce874143
zenghongyujia123 <1963968619@qq.com>
2018-02-27 21:55:03 +0800

    Update for test

--- web/c_wechat/controllers/page_supply_list.client.controller.js
+++ web/c_wechat/controllers/page_supply_list.client.controller.js
@@ -44,7 +44,7 @@ $(function () {
         var obj = $(
           '<a href="/page_wechat/page_supply_detail?supply_id=' + item._id + '" class="weui-media-box weui-media-box_appmsg purchases-list-item">' +
           '  <div class="weui-media-box__hd">' +
-          '    <img class="weui-media-box__thumb" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1516813447292&di=5f4eaade66c430dd3a958c2cfac95425&imgtype=0&src=http%3A%2F%2Fpic32.photophoto.cn%2F20140821%2F0006019010973602_b.jpg">' +
+          '    <img class="weui-media-box__thumb" src="' + item.photos[0] + '">' +
           '  </div>' +
           '  <div class="weui-media-box__bd">' +
           '    <div class="title1">' + item.goods_name +
