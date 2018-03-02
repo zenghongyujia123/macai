3f4e3a45a1cc6ca2293039f655e572435abb83f0
zenghongyujia123 <1963968619@qq.com>
2018-03-03 00:15:31 +0800

    Update for test

--- web/c_wechat/controllers/page_purchases_list.client.controller.js
+++ web/c_wechat/controllers/page_purchases_list.client.controller.js
@@ -140,7 +140,7 @@ $(function () {
           '    <div class="title1">' + (item.goods_class || '') + item.goods_category + item.goods_brand +
           '    </div>' +
           '    <div class="title2">' + item.send_province + item.send_city + ' ' + (item.nickname || '-') + '</div>' +
-          ((item.user && item.user.personal_auth_stauts === 'authed') ? '<span class="tag blue">认证用户</span>' : '<span class="tag">浦东</span>') +
+          ((item.user && item.user.personal_auth_stauts === 'authed') ? '<span class="tag blue">认证用户</span>' : '<span class="tag">普通用户</span>') +
           ((item.user && item.user.vip_user) ? '<span class="tag orange">vip用户</span>' : '') +
           // '    <span class="tag orange">企业</span>' +
           '    <div class="item-bottom">' +
