361d31bb4e94adec03d14a16eac53c307cec5a0e
zenghongyujia123 <1963968619@qq.com>
2018-03-06 16:36:14 +0800

    Update for test

--- web/c_wechat/controllers/page_purchases_list.client.controller.js
+++ web/c_wechat/controllers/page_purchases_list.client.controller.js
@@ -155,7 +155,7 @@ $(function () {
           '        <span class="price-unit">' + item.price_unit + '</span>' +
           '      </div>' +
           '      <div class="time">' +
-          '        ' + (new Date(item.create_time).getMonth() + 1) + '月' + new Date(item.create_time).getDate() + '日' +
+          '        最后刷新时间：' + (new Date(item.create_time).getMonth() + 1) + '月' + new Date(item.create_time).getDate() + '日' +
           '      </div>' +
           '    </div>' +
           '  </div>' +
