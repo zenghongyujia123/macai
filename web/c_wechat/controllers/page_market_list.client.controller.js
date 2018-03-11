1e42a62885ec96d9397a7aa5259f4e663c99b9ef
zenghongyujia123 <1963968619@qq.com>
2018-03-11 16:54:26 +0800

    Update for test

--- web/c_wechat/controllers/page_market_list.client.controller.js
+++ web/c_wechat/controllers/page_market_list.client.controller.js
@@ -205,7 +205,7 @@ $(function () {
           '     </div>';
         // '     <div class="title2">' + item.main_goods + '</div>';
         item.list.forEach(function (o) {
-          str += '<div class="item-bottom">' +
+          str += '<div class="item-bottom has-bottom-border">' +
             '       <div class="brand">' + o.main_goods + '</div>' +
             '       <div class="last-day-price">' + o.last_day_price + '</div>' +
             '       <div class="today-price">' + o.price + '</div>' +
