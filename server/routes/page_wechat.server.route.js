c1a79f2dbbc56d64b84e17a86ae08e7fe86aca05
zenghongyujia123 <1963968619@qq.com>
2018-03-12 02:23:27 +0800

    Update for test

--- server/routes/page_wechat.server.route.js
+++ server/routes/page_wechat.server.route.js
@@ -21,12 +21,12 @@ module.exports = function (app) {
   app.route('/page_wechat/page_supply_my_list').get(index.page_supply_my_list);
   app.route('/page_wechat/page_supply_detail').get(goodsFilter.requireSupply, index.page_supply_detail);
 
-  // app.route('/page_market_list').get(userFilter.requireUser, index.page_market_list);
+  app.route('/page_market_list').get(userFilter.requireUser, index.page_market_list);
   // app.route('/page_market_detail').get(index.page_market_detail);
   app.route('/page_market_detail').get(userFilter.requireUser, index.page_market_detail);
 
 
-  app.route('/page_market_list').get(index.page_market_list);
+  // app.route('/page_market_list').get(index.page_market_list);
   app.route('/page_wechat/page_my_main').get(userFilter.requireUser, index.page_my_main);
   app.route('/page_wechat/page_my_auth').get(userFilter.requireUser, index.page_my_auth);
   app.route('/page_wechat/page_my_vip').get(userFilter.requireUser, index.page_my_vip);
