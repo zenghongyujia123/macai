09595f5c692477ffade673cdc1804a26c055051a
zenghongyujia123 <1963968619@qq.com>
2018-03-22 12:03:25 +0800

    Update for test

--- server/routes/page_wechat.server.route.js
+++ server/routes/page_wechat.server.route.js
@@ -11,14 +11,14 @@ module.exports = function (app) {
   app.route('/page_wechat/page_home').get(index.page_home);
   app.route('/page_wechat/page_signin').get(index.page_signin);
 
-  app.route('/page_wechat/page_purchases_create_main').get(index.page_purchases_create_main);
-  // app.route('/page_wechat/page_purchases_create_main').get(userFilter.requireUser, index.page_purchases_create_main);
+  // app.route('/page_wechat/page_purchases_create_main').get(index.page_purchases_create_main);
+  app.route('/page_wechat/page_purchases_create_main').get(userFilter.requireUser, index.page_purchases_create_main);
   app.route('/page_wechat/page_purchases_list').get(index.page_purchases_list);
   app.route('/page_wechat/page_purchases_my_list').get(index.page_purchases_my_list);
   app.route('/page_wechat/page_purchases_detail').get(goodsFilter.requirePurchases, index.page_purchases_detail);
 
-  // app.route('/page_wechat/page_supply_create_main').get(userFilter.requireUser, index.page_supply_create_main);
-  app.route('/page_wechat/page_supply_create_main').get(index.page_supply_create_main);
+  app.route('/page_wechat/page_supply_create_main').get(userFilter.requireUser, index.page_supply_create_main);
+  // app.route('/page_wechat/page_supply_create_main').get(index.page_supply_create_main);
   app.route('/page_wechat/page_supply_list').get(index.page_supply_list);
   app.route('/page_wechat/page_supply_my_list').get(index.page_supply_my_list);
   app.route('/page_wechat/page_supply_detail').get(goodsFilter.requireSupply, index.page_supply_detail);
